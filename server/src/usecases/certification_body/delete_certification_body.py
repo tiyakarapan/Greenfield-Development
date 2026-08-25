8747949ac40f0da46ddd0878186ed6afad23d347
Tiya K <tiya.karapan@gmail.com>
2026-08-25 17:00:36 +0200

    Fixed delete usecases for certification_body and enrollment

--- server/src/usecases/certification_body/delete_certification_body.py
+++ server/src/usecases/certification_body/delete_certification_body.py
@@ -2,4 +2,5 @@ from ...gateways.certification_body_gateway import CertificationBodyGateway
 
 def delete_certification_body(id):
     certification_body_gateway = CertificationBodyGateway()
-    return certification_body_gateway.delete(id)
+    certification_body_gateway.delete(id)
+    return { "deleted": True }
--- server/src/usecases/enrollments/delete_enrollment.py
+++ server/src/usecases/enrollments/delete_enrollment.py
@@ -2,4 +2,5 @@ from ...gateways.enrollment_gateway import EnrollmentGateway
 
 def delete_enrollment(id):
     enrollment_gateway = EnrollmentGateway()
-    return enrollment_gateway.delete(id)
+    enrollment_gateway.delete(id)
+    return { "deleted": True }
