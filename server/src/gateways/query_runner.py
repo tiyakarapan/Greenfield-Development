8e9baa3c17805d2999e9f30b3021ff8c2b1e469e
SeanNeal <seanneal572@gmail.com>
2026-08-25 19:26:55 +0400

    Simplified query runner

--- server/src/gateways/query_runner.py
+++ server/src/gateways/query_runner.py
@@ -1,5 +1,6 @@
 from typing import List, Any, Dict
 from psycopg import connect
+from psycopg.rows import dict_row
 
 class QueryRunner:
     def __init__(self, table: str, pk: str | List[str]):
@@ -8,40 +9,25 @@ class QueryRunner:
         self.connection_string = "host='localhost' dbname='itca' user='postgres' password='itca' port=5432"
     
     def list_all(self, columns: List[str]):
-        with connect(self.connection_string) as connection:
+        with connect(self.connection_string, row_factory=dict_row) as connection:
             with connection.cursor() as cursor:
-                cursor.execute(f"SELECT {", ".join(columns)} FROM {self.table}")
-                result = cursor.fetchall()
-
-                return [dict([(columns[i], row[i]) for i in range(len(columns))]) for row in result]
+                cursor.execute(f"SELECT * FROM {self.table}")
+                return cursor.fetchall()
 
     def insert(self, values: Dict[str, Any]):
-        with connect(self.connection_string) as connection:
+        with connect(self.connection_string, row_factory=dict_row) as connection:
             with connection.cursor() as cursor:
                 columns = list(values.keys())
                 
                 cursor.execute(f"""
                     INSERT INTO {self.table} ({", ".join(columns)}) 
                     VALUES ({", ".join("%s" for _ in range(len(values)))}) 
-                    RETURNING {", ".join(self.pk)}, {", ".join(columns)}
+                    RETURNING *
                 """, tuple(values.values()))
-                row = cursor.fetchone()
-
-                if not row:
-                    raise Exception("Failed to insert value")
-
-                result = {}
-
-                for i in range(len(self.pk)):
-                    result[self.pk[i]] = row[i]
-
-                for i in range(len(self.pk), len(columns)):
-                    result[columns[i]] = row[i]
-
-                return result
+                return cursor.fetchone()
 
     def update(self, id: Any | List[Any], values: Dict[str, Any]):
-        with connect(self.connection_string) as connection:
+        with connect(self.connection_string, row_factory=dict_row) as connection:
             with connection.cursor() as cursor:
                 columns = list(values.keys())
 
@@ -51,25 +37,12 @@ class QueryRunner:
                     UPDATE {self.table}
                     SET {", ".join(f"{column} = %s" for column in columns)}
                     WHERE {" AND ".join(f"{pk} = %s" for pk in self.pk)}
-                    RETURNING {", ".join(self.pk)}, {", ".join(columns)}
+                    RETURNING *
                 """, tuple(list(values.values()) + id_values))
-                row = cursor.fetchone()
-
-                if not row:
-                    raise Exception("Unexpected error")
-
-                result = {}
-
-                for i in range(len(self.pk)):
-                    result[self.pk[i]] = row[i]
-
-                for i in range(len(self.pk), len(columns)):
-                    result[columns[i]] = row[i]
-
-                return result
+                return cursor.fetchone()
 
     def delete(self, id: Any):
-        with connect(self.connection_string) as connection:
+        with connect(self.connection_string, row_factory=dict_row) as connection:
             with connection.cursor() as cursor:
                 id_values = id if isinstance(id, list) else [id]
                 cursor.execute(f"""
@@ -78,7 +51,7 @@ class QueryRunner:
                 """, tuple(id_values))
 
     def execute_raw(self, query: str, params: List[Any]):
-        with connect(self.connection_string) as connection:
+        with connect(self.connection_string, row_factory=dict_row) as connection:
             with connection.cursor() as cursor:
                 cursor.execute(query, tuple(params))
                 return cursor.fetchall()
