d5d59942be61b78e77a0c5cf37a393554e18a46e
Tiya K <tiya.karapan@gmail.com>
2026-08-26 15:24:52 +0200

    Exam attempt controller fixed

--- server/src/controllers/exam_attempt_controller.py
+++ server/src/controllers/exam_attempt_controller.py
@@ -2,20 +2,20 @@ from ..app import App
 from ..usecases.exam_attempt import get_all_exam_attempts, create_exam_attempt, update_exam_attempt, delete_exam_attempt
 
 def init_exam_attempt_controller(app: App):
-    @app.get("exam-attempt/")
+    @app.get("exam_attempt/")
     def get_all_exam_attempts_endpoint():
         return get_all_exam_attempts()
 
-    @app.post("exam-attempt/")
+    @app.post("exam_attempt/")
     def create_exam_attempt_endpoint():
         values = app.get_request_body()
         return create_exam_attempt(values)
 
-    @app.put("exam-attempt/<int:id>")
+    @app.put("exam_attempt/<int:id>")
     def update_exam_attempt_endpoint(id):
         values = app.get_request_body()
         return update_exam_attempt(id, values)
 
-    @app.delete("exam-attempt/<int:id>")
+    @app.delete("exam_attempt/<int:id>")
     def delete_exam_attempt_endpoint(id):
         return delete_exam_attempt(id)
